e491aac18e487ab5817a0a03ea626fb6feec4928
Sindre Lindstad <>
2021-04-08 12:07:51 +0200

    Update provider version constraints

--- modules/init/versions.tf
+++ modules/init/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.13.2, <0.14"
   required_providers {
-    google = "~> 3.5"
-    kubernetes = "~> 1.9"
+    google = ">= 3.5"
+    kubernetes = ">= 1.9"
   }
 }
