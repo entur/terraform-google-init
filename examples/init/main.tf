cfd20539a40cc8c3dd90ca4d9d958f2f8ea21377
Sindre Lindstad <>
2022-07-01 19:39:45 +0200

    feat: release-please docs bump

--- examples/init/main.tf
+++ examples/init/main.tf
@@ -1,10 +1,15 @@
-provider "google" {}
+# x-release-please-start-version
 
-module "example" {
-  source      = "../../modules/init"
-  app_id      = "potato"
+module "init" {
+  # This is an example only; if you're adding this block to a live configuration,
+  # make sure to use the latest release of the init module, found here:
+  # https://github.com/entur/terraform-google-init/releases
+  source      = "github.com/entur/terraform-google-init//modules/init?ref=v0.1.0"
+  app_id      = "tfmodules"
   environment = "dev"
 }
+
+# x-release-please-end
 
 output "module" {
   value = module.example
