24d9913c65fc60c30552169bed54282ad805de0f
Glenn Terjesen <glenn.terjesen@gmail.com>
2025-03-19 17:56:32 +0100

    chore: migrate away from gh-workflows

--- examples/init/main.tf
+++ examples/init/main.tf
@@ -1,9 +1,5 @@
-# ci: x-release-please-start-version
-
 module "init" {
-  source      = "github.com/entur/terraform-google-init//modules/init?ref=v1.0.0"
+  source      = "github.com/entur/terraform-google-init//modules/init?ref=v1"
   app_id      = "tfmodules"
   environment = "dev"
 }
-
-# ci: x-release-please-end
